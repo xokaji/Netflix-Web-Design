4e2c662cc25c850bc3430c14939607d6cf0c3334
xokaji <rmahiti2@gmail.com>
2023-12-14 00:08:34 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import "./App.scss"
+
+import Register from './register/Register'
+import Login from './login/Login'
 import Home from './home/Home'
+import Watch from './pages/watch/Watch'
+
 
 const App = () => {
   return (
-    <Home/>
+    
+    <Login/>
+
+    
+    
   )
 }
 
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -14,5 +14,6 @@ const Home = () => {
       <List/>
     </div>
   )
-  }
+}
+
 export default Home
